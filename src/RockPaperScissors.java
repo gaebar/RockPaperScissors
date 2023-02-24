4f778c53981701f8fc680adc58a5909fc8ae1d73
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-02-24 15:08:23 -0800

    working on if else conditions

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.text.PlainDocument;
+
 public class RockPaperScissors {
     public static void main(String[] args) {
         String[] rps = {"r", "p", "s"};
@@ -13,9 +15,24 @@ public class RockPaperScissors {
         while(true){
             System.out.println("Please enter your move (r, p, or s)");
             playerMove = scanner.nextLine();
-            if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals(|"s")){
+            if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")){
                 break;
             }
+            System.out.println(playerMove + " is not a valid move.");
+        }
+        System.out.println("Computer played: " + computerMove);
 
+        if (playerMove.equals(computerMove)) {
+            System.out.println("The game was a tie!");
+        }
+        else if (playerMove.equals("r")) {
+            if (computerMove.equals("p")) {
+                System.out.println("You lose!");
+            }
+            else if (computerMove.equals("s")) {
+                System.out.println("You win!");
+            }
+        }
     }
 }
+;
