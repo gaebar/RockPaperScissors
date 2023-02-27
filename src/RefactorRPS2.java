8badcd2067ae28f16a4eee7f0a52a33e15003036
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-02-27 12:19:35 -0800

    create a new refactor file

---
+++ src/RefactorRPS2.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RefactorRPS2 {
+    public static void main(String[] args) {
+
+        // Create a new Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
